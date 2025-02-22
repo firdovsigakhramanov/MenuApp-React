6c1ac77678831f3a71a583af12487088ce3d2bdb
Firdovsi <firdovsigakhramanov@gmail.com>
2025-02-22 18:41:27 +0400

    LOGIN PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path='' element={<HomePage />} />
-          <Route path='login' element={<Login />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
           <Route path='interior' element={<Interior />} />
